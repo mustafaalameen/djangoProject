80bb8b052343ca6667881319bb73c30876883b65
Mustafa Al-ameen Mofeyisayo <mustafaalameen2017@gmail.com>
2021-04-26 19:20:17 +0100

    heroku config

--- main_file/settings.py
+++ main_file/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.path.join('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.path.join('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost','127.0.0.1']
 
 
 # Application definition
