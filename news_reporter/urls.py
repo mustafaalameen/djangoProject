9c8be02e16fb25d927ca8e7ab5501dc52be85a2c
Mustafa Al-ameen Mofeyisayo <mustafaalameen2017@gmail.com>
2021-04-27 21:06:56 +0100

    updated view

--- news_reporter/urls.py
+++ news_reporter/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('news_report/<int:pk>/edit/', NewsUpdateView.as_view(), name='news-update'),
     path('news_report/create/', NewsCreateView.as_view(), name='news-create'),
     path('news_report/<int:pk>/', NewsDetailsView.as_view(), name='news-detail'),
-    path('news_report', NewsListView.as_view(), name='news-home'),
+    path('news_report/', NewsListView.as_view(), name='news-home'),
     path ('', HomeView.as_view(), name='home')
     ]
